ba7d00367363493d886195e87b6d0a04d1d84a0c
linyang@vgpay.co <cynick@qq.com>
2023-03-16 11:42:01 +0800

    up

--- Common/Exchange/BinanceFuturesUSDT_MExchange.cs
+++ Common/Exchange/BinanceFuturesUSDT_MExchange.cs
@@ -674,7 +674,7 @@ namespace CEF.Common.Exchange
             if (startUserStreamResult.Success)
             {
                 _listenKey = startUserStreamResult.Data;
-                JobHelper.SetIntervalJob(async () => await KeepAliveUserStreamAsync(), TimeSpan.FromMinutes(15)); ;
+                JobHelper.SetIntervalJob(async () => await KeepAliveUserStreamAsync(), TimeSpan.FromMinutes(5));
             }
             else
                 this._logger.LogError($"Binance StartUserStreamAsync error, {startUserStreamResult.Error?.Code} {startUserStreamResult.Error?.Message}!");
