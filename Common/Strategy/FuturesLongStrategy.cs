942fed9a83f05ff91d14e2aafeb7d5041a02a827
linyang@vgpay.co <cynick@qq.com>
2023-04-06 14:29:33 +0800

    up

--- Common/Strategy/FuturesLongStrategy.cs
+++ Common/Strategy/FuturesLongStrategy.cs
@@ -40,18 +40,21 @@ namespace CEF.Common.Strategy
                 {
                     if (((future.LastTransactionOpenPrice - per15MinuteIndexedOhlcv.Close) / future.LastTransactionOpenPrice) > future.SafetyOrderPriceDeviation &&
                         ((future.LastTransactionOpenPrice - per15MinuteIndexedOhlcv.Close) / future.LastTransactionOpenPrice) < 0.03m &&
-                       per15MinuteIndexedOhlcv.Prev.Close > per15MinuteIndexedOhlcv.Prev.Open)
+                       per15MinuteIndexedOhlcv.Prev.Close > per15MinuteIndexedOhlcv.Prev.Open &&
+                       per15MinuteIndexedOhlcv.Close > per15MinuteIndexedOhlcv.Prev.Close)
                         await openFunc?.Invoke(future.Symbol, OrderType.Limit, Side, future.SafetyOrderSize);
                     else if (((future.LastTransactionOpenPrice - per15MinuteIndexedOhlcv.Close) / future.LastTransactionOpenPrice) > 0.03m &&
                         fourHourlyIndexedOhlcv.Prev.Close > fourHourlyIndexedOhlcv.Prev.Open && 
-                        per15MinuteIndexedOhlcv.Prev.Close > per15MinuteIndexedOhlcv.Prev.Open)
+                        per15MinuteIndexedOhlcv.Prev.Close > per15MinuteIndexedOhlcv.Prev.Open &&
+                       per15MinuteIndexedOhlcv.Close > per15MinuteIndexedOhlcv.Prev.Close)
                         await openFunc?.Invoke(future.Symbol, OrderType.Limit, Side, future.SafetyOrderSize);
                 }
                 else if (future.OrdersCount < (future.MaxSafetyOrdersCount + 1))
                 {
                     if (((future.LastTransactionOpenPrice - per15MinuteIndexedOhlcv.Close) / future.LastTransactionOpenPrice) > (future.SafetyOrderPriceDeviation * future.SafetyOrderPriceScale *(future.OrdersCount - 1)) &&
                        fourHourlyIndexedOhlcv.Prev.Close > fourHourlyIndexedOhlcv.Prev.Open && 
-                       per15MinuteIndexedOhlcv.Prev.Close > per15MinuteIndexedOhlcv.Prev.Open)
+                       per15MinuteIndexedOhlcv.Prev.Close > per15MinuteIndexedOhlcv.Prev.Open &&
+                       per15MinuteIndexedOhlcv.Close > per15MinuteIndexedOhlcv.Prev.Close)
                         await openFunc?.Invoke(future.Symbol, OrderType.Limit, Side, future.SafetyOrderSize * future.SafetyOrderVolumeScale * (future.OrdersCount - 1));
                 }
             }
--- Common/Strategy/FuturesShortStrategy.cs
+++ Common/Strategy/FuturesShortStrategy.cs
@@ -41,18 +41,21 @@ namespace CEF.Common.Strategy
                 {
                     if (((per15MinuteIndexedOhlcv.Close - future.LastTransactionOpenPrice) / future.LastTransactionOpenPrice) > future.SafetyOrderPriceDeviation &&
                         ((per15MinuteIndexedOhlcv.Close - future.LastTransactionOpenPrice) / future.LastTransactionOpenPrice) < 0.03m &&
-                       per15MinuteIndexedOhlcv.Prev.Close < per15MinuteIndexedOhlcv.Prev.Open)
+                       per15MinuteIndexedOhlcv.Prev.Close < per15MinuteIndexedOhlcv.Prev.Open &&
+                       per15MinuteIndexedOhlcv.Close < per15MinuteIndexedOhlcv.Prev.Close)
                         await openFunc?.Invoke(future.Symbol, OrderType.Limit, Side, future.SafetyOrderSize);
                     else if (((per15MinuteIndexedOhlcv.Close - future.LastTransactionOpenPrice) / future.LastTransactionOpenPrice) > 0.03m &&
                         fourHourlyIndexedOhlcv.Prev.Close < fourHourlyIndexedOhlcv.Prev.Open &&
-                        per15MinuteIndexedOhlcv.Prev.Close < per15MinuteIndexedOhlcv.Prev.Open)
+                        per15MinuteIndexedOhlcv.Prev.Close < per15MinuteIndexedOhlcv.Prev.Open &&
+                       per15MinuteIndexedOhlcv.Close < per15MinuteIndexedOhlcv.Prev.Close)
                         await openFunc?.Invoke(future.Symbol, OrderType.Limit, Side, future.SafetyOrderSize);
                 }
                 else if (future.OrdersCount < (future.MaxSafetyOrdersCount + 1))
                 {
                     if (((per15MinuteIndexedOhlcv.Close - future.LastTransactionOpenPrice) / future.LastTransactionOpenPrice) > (future.SafetyOrderPriceDeviation * future.SafetyOrderPriceScale * (future.OrdersCount - 1)) &&
                        fourHourlyIndexedOhlcv.Prev.Close < fourHourlyIndexedOhlcv.Prev.Open &&
-                       per15MinuteIndexedOhlcv.Prev.Close < per15MinuteIndexedOhlcv.Prev.Open)
+                       per15MinuteIndexedOhlcv.Prev.Close < per15MinuteIndexedOhlcv.Prev.Open &&
+                       per15MinuteIndexedOhlcv.Close < per15MinuteIndexedOhlcv.Prev.Close)
                         await openFunc?.Invoke(future.Symbol, OrderType.Limit, Side, future.SafetyOrderSize * future.SafetyOrderVolumeScale * (future.OrdersCount - 1));
                 }
             }
